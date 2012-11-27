256fb72eb49fe8c711b4a96b0d370082fade6972
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2012-11-27 12:52:30 +0100

    Added option variable MAIL_RECIPIENTS + switched to cbcrg-lab

--- epilog.sh
+++ epilog.sh
@@ -20,7 +20,7 @@ cd $OLDPWD
 #
 # Upload the result
 #
-s3cmd --rr --acl-public put $FULLZIP s3://cbcrg-eu/$ZIP
+s3cmd --rr --acl-public put $FULLZIP s3://cbcrg-lab/$ZIP
 
 #
 # Upload logs
@@ -30,8 +30,8 @@ bash qupload.sh
 #
 # Send notification email
 #
-MAIL_BODY="$PRJNAME job completed\n\nDownload the result at this link http://cbcrg-eu.s3.amazonaws.com/$ZIP\n\nBye"
-MAIL_RECIPIENTS="paolo.ditommaso@gmail.com chang.jiaming@gmail.com"
+MAIL_BODY="$PRJNAME job completed\n\nDownload the result at this link http://cbcrg-lab.s3.amazonaws.com/$ZIP\n\nBye"
+MAIL_RECIPIENTS=${MAIL_RECIPIENTS:-'paolo.ditommaso@gmail.com'}
 MAIL_FROM="tcoffee.msa@gmail.com"
 MAIL_SUBJECT="$PRJNAME terminated ($$)!"
 echo -e "$MAIL_BODY" | ses-send-email.pl -s "$MAIL_SUBJECT" -f $MAIL_FROM $MAIL_RECIPIENTS
