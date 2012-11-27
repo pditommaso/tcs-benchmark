48e98efee6837d08221b2029927602660ac9b97e
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2012-11-27 12:55:34 +0100

    Added some vas

---
+++ prolog_aws.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export DB_FOLDER=/share/big/data
+export DATA_FOLDER=/share/result
+export LOG_FOLDER=/vol/logs
+export ZIP_FOLDER=/vol
+
+export RUN_MODE=normal
+export COUNT=100
+export QDELAY=120
+export MAIL_RECIPIENTS='paolo.ditommaso@gmail.com chang.jiaming@gmail.com'
+
+
